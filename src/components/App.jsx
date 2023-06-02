e48ac95551eba06d534755da30919bed11cf796c
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-06-02 23:45:57 +0300

    test route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import MoviesPage from "./Pages/MoviesPage";
 
 export default function App() {
-  
+    return <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/movies" element={<MoviesPage/>}></Route>
+  </Routes>
 };
